d2732e15dfc94949c59c40c9fe1274e43843afc9
Usman Javed <usmanjaved0816@gmail.com>
2024-10-02 16:08:40 +0500

    finding elements premitives

---
+++ JavaScript/finding-elements.js
@@ -0,0 +1,16 @@
+ 
+
+    //    finding elements(primitives) in array 
+
+    const number = [1,2,3,4,5];
+    console.log(number.indexOf(6));
+
+    console.log(number.indexOf(2));
+
+    console.log(number.lastIndexOf(5));
+
+    console.log(number.indexOf(2) !== -1);  // old method
+
+    console.log(number.includes(4));  // new method
+
+
