6fb05141f5ac980998780ae75a39fc538ee1ddaa
Usman Javed <usmanjaved0816@gmail.com>
2024-10-02 15:42:32 +0500

    adding elements into array

---
+++ JavaScript/adding-elements.js
@@ -0,0 +1,22 @@
+   
+
+// adding elements in array
+
+const number = [3,4];
+
+
+// adding element into array in last 
+
+number.push (5,6)
+
+// adding element into array in start 
+
+number.unshift (1, 2);
+
+
+// adding element into array in middle
+
+number.splice (2 , 0 , 'a', 'b');
+
+
+console.log(number);
