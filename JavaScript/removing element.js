34b2cfa31823153cb6f92c67efb500c41fb44f39
Usman Javed <usmanjaved0816@gmail.com>
2024-10-02 17:15:16 +0500

    removing element from array

---
+++ JavaScript/removing element.js
@@ -0,0 +1,22 @@
+     
+
+    //  removing element
+
+     const numbers = [1,2,3,4,5];
+
+    //  remove element from end 
+
+    // const last =  numbers.pop();
+    
+    
+    // remove element form start 
+    
+    // const start = numbers.shift();
+
+    
+    // remove number in the middle
+    
+    numbers.splice (2,2);
+
+    console.log(numbers);
+    
