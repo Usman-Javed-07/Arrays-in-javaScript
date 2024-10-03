f434cdfe19ea4d84299f86b512c5b85ac6004f04
Usman Javed <usmanjaved0816@gmail.com>
2024-10-03 13:07:40 +0500

    iterating an array

--- JavaScript/iterating an array.js
+++ JavaScript/iterating an array.js
@@ -1,1 +1,17 @@
 //   iterating an array 
+   
+// first way 
+ const numbers = [1,2,3,7]
+ for (let number of numbers) 
+    console.log(number);
+
+//  second way 
+
+//   numbers.forEach (function (number) {
+//     console.log(number);
+    
+//   });
+
+//  more simple way 
+
+numbers.forEach ( (number, index) => console.log(index, number));
