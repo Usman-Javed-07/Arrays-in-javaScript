145be8f2422ff13fc924b5515a0767229714c80f
Usman Javed <usmanjaved0816@gmail.com>
2024-10-03 14:10:49 +0500

    testing all element in array

---
+++ JavaScript/testing the element of an array.js
@@ -0,0 +1,15 @@
+ // testing the element of an array 
+
+ // for all positive 
+  
+ const number = [1,2,-1,3,4];
+
+//  every 
+// some
+
+     const allpositve = number.some(function(value){
+        return value >= 0;
+        
+     });
+     
+     console.log(allpositve);
